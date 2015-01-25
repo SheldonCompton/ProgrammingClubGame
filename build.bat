f1e45b9a2536fe92377d38b74ce8fbaf844e482e
SheldonCompton <SheldonCompton23@gmail.com>
2015-01-25 09:29:52 -0600

    Correcting path mistake in build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir ..\build
 pushd ..\build
-cl -Zi ..\Project/code/Main.cpp user32.lib Gdi32.lib Xinput9_1_0.lib
+cl -Zi ..\ProgrammingClubGame/code/Main.cpp user32.lib Gdi32.lib Xinput9_1_0.lib
 popd
-PAUSE
+
