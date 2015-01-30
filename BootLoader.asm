bd2a0073e7ab5f8119abe3d7de5362139b527247
SheldonCompton <SheldonCompton23@gmail.com>
2015-01-30 17:58:37 -0600

    Better string printing
    A nicer looking way to print the string.

--- BootLoader.asm
+++ BootLoader.asm
@@ -1,7 +1,7 @@
 org 7C00h
 
 jmp 0x0:start
-
+LoadingMessage db 'Loading Kernel...',0
 start:
 cli
 mov ax, 0x9000 ;Set up stack
@@ -9,56 +9,19 @@ mov ss, ax     ;Tell processor where stack is
 mov sp, 0xFB00 ;Set stack offset
 sti
 
+jmp printMessage
 
-mov ah, 0Eh ;Tell bios we're writing a char to screen!
-mov al, 'Y' ;Load a new character into memory
-int 10h     ;Call BIOS video interrupt
-mov al, 'o'
+printMessage:
+mov ah, 0eh
+mov si, LoadingMessage
+.loop:
+lodsb
+cmp ah,0
+jz .done
 int 10h
-mov al, 'u'
-int 10h
-mov al, ' '
-int 10h
-mov al, 'c'
-int 10h
-mov al, 'a'
-int 10h
-mov al, 'n'
-int 10h
-mov al, 't'
-int 10h
-mov al, ' '
-int 10h
-mov al, 'd'
-int 10h
-mov al, 'o'
-int 10h
-mov al, ' '
-int 10h
-mov al, 't'
-int 10h
-mov al, 'h'
-int 10h
-mov al, 'i'
-int 10h
-mov al, 's'
-int 10h
-mov al, ' '
-int 10h
-mov al, 'i'
-int 10h
-mov al, 'n'
-int 10h
-mov al, ' '
-int 10h
-mov al, 'c'
-int 10h
-mov al, '+'
-int 10h
-mov al, '+'
-int 10h
-
+jmp .loop
+.done:
 jmp $
 
 times 510-($-$$) db 0 ;Fill rest of sector up with 0s to make this 512B (a sector)
-dw 0xAA55             ;Let BIOS know this is an OS
+dw 0xAA55	      ;Let BIOS know this is an OS
