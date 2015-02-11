2aa774f08fdb7f8f9639a8d882b6c8f4e17f18ee
SheldonCompton <SheldonCompton23@gmail.com>
2015-02-11 15:56:48 -0600

    Unconfigured FDD-bootloader Good news: We have a working bootloader. Bad news: I'm an idiot. Turns out the HDD-Bootloader never actually worked.  I just accidently copied it into the MBR, so I looked fine to me.  For the FDD-BootLoader I added a OEM block (I should look up what that acronym means).  I haven't configured it properly yet and honestly I'm not exactly sure what it does beside that you need one for floppy emulation. Also I think we might be able to ditch HDD emulation, but like most things right now I'm not completely sure.

---
+++ FDD-BootLoader.asm
@@ -0,0 +1,79 @@
+use16
+org 7C00h
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;BOOTLOADER FOR FDD AND DRIVES EMULATING FDD;
+;DO NOT MODIFY REGISTER DL                      ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+jmp start
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;I HATE BIOS DESIGNERS RIGHT NOW.;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+times 0x0b-($$-$) db 0
+;;;;;;;;;;;
+;OEM BLOCK;
+;;;;;;;;;;;
+
+bpbBytesPerSector:  	DW 512
+bpbSectorsPerCluster: 	DB 1
+bpbReservedSectors: 	DW 1
+bpbNumberOfFATs: 	DB 2
+bpbRootEntries: 	DW 224
+bpbTotalSectors: 	DW 2880
+bpbMedia: 	        DB 0xF0
+bpbSectorsPerFAT: 	DW 9
+bpbSectorsPerTrack: 	DW 18
+bpbHeadsPerCylinder: 	DW 2
+bpbHiddenSectors: 	DD 0
+bpbTotalSectorsBig:     DD 0
+bsDriveNumber: 	        DB 0
+bsUnused: 	        DB 0
+bsExtBootSignature: 	DB 0x29
+bsSerialNumber:	        DD 0xa0a1a2a3
+bsVolumeLabel: 	        DB "MOS FLOPPY "
+bsFileSystem: 	        DB "FAT12   "
+
+start:
+
+cli
+mov ax, 0x0200	;start the stack after the bootloader
+mov ss, ax	
+mov ax, 0xC00	;make the stack 3k big later this will need to be changed and hardware detection added
+mov ax, sp	;but for now this will work.
+mov ax, ds
+sti
+
+mov ah, 0x0e
+mov al, '1'
+int 0x10
+
+mov ah, 0x42
+mov si, DiskAddressPacket
+int 0x13
+jc error
+
+
+jmp	0xA00:0x0200
+
+error:
+mov ah, 0x0e
+mov al, 'E'	
+int 0x10
+
+
+DiskAddressPacket:
+db 0x10		;size of packet
+db 0x0		;reserved
+dw 0x1		;number of blocks to transfer
+dw 0xA00	;where in memory to write to
+dw 0x200	;offset
+dq 0x2		;starting block to read
+dd 0x0
+
+times 510-($-$$) db 0 ;Fill rest of sector up with 0s to make this 512B (a sector)
+dw 0xAA55             ;Let BIOS know this is an OS
+
+
--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-cd /home/sheldon/Desktop/fasm
+cd /home/sheldon/Work/fasm
 
-./fasm ../Git/ProgrammingClubGame/BootLoader0.03.asm
+./fasm ../Git/ProgrammingClubGame/FDD-BootLoader.asm
 ./fasm ../Git/ProgrammingClubGame/Kernel0.01.asm
  
-cd /home/sheldon/Desktop/Git/ProgrammingClubGame
+cd /home/sheldon/Work/Git/ProgrammingClubGame
 
-dd if=/home/sheldon/Desktop/Git/ProgrammingClubGame/BootLoader0.03.bin of=/dev/sdc obs=512 bs=512 count=1
-dd if=/home/sheldon/Desktop/Git/ProgrammingClubGame/Kernel0.01.bin of=/dev/sdc obs=512 bs=512 count=1
+dd if=/home/sheldon/Work/Git/ProgrammingClubGame/FDD-BootLoader.bin of=/dev/sdc obs=512 bs=512 count=1
+dd if=/home/sheldon/Work/Git/ProgrammingClubGame/Kernel0.01.bin of=/dev/sdc obs=512 bs=512 count=1
 
-rm /home/sheldon/Desktop/Git/ProgrammingClubGame/BootLoader0.03.bin
-rm /home/sheldon/Desktop/Git/ProgrammingClubGame/Kernel0.01.bin
+rm /home/sheldon/Work/Git/ProgrammingClubGame/FDD-BootLoader.bin
+rm /home/sheldon/Work/Git/ProgrammingClubGame/Kernel0.01.bin
