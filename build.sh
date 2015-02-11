9cc0253637b1888a56247f35056e56f98314414d
SheldonCompton <SheldonCompton23@gmail.com>
2015-02-11 16:12:08 -0600

    Update build.sh
    Turns out I'm twice the idiot because I did it again.  Yea, we really don't have a working bootloader and it's starting to feel like we never will.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd /home/sheldon/Work/fasm
 cd /home/sheldon/Work/Git/ProgrammingClubGame
 
 dd if=/home/sheldon/Work/Git/ProgrammingClubGame/FDD-BootLoader.bin of=/dev/sdc obs=512 bs=512 count=1
-dd if=/home/sheldon/Work/Git/ProgrammingClubGame/Kernel0.01.bin of=/dev/sdc obs=512 bs=512 count=1
+dd if=/home/sheldon/Work/Git/ProgrammingClubGame/Kernel0.01.bin of=/dev/sdc obs=512 seek=1 bs=512 count=1
 
 rm /home/sheldon/Work/Git/ProgrammingClubGame/FDD-BootLoader.bin
 rm /home/sheldon/Work/Git/ProgrammingClubGame/Kernel0.01.bin
